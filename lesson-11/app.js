a77d77a1d6a8da01335efb85ad10895e34a45669
QuyDuongNgo <Quyduongngo2000@gmail.com>
2023-11-21 10:06:44 +0700

    https://bap.udemy.com/course/build-web-apps-with-vuejs-firebase/learn/lecture/23483040\#overview

--- lesson-11/app.js
+++ lesson-11/app.js
@@ -5,12 +5,24 @@ const app = Vue.createApp({
             showBook: true,
             title: 'The Final Empire',
             author: 'QuyDN',
-            age: 24
+            age: 24,
+            x: 0,
+            y: 0
         }
     },
    methods: {
     toggleShowBook() {
         this.showBook = !this.showBook
+    },
+    handleEvent(e, data) {
+        console.log(e, e.type);
+        if (data) {
+            console.log(data);
+        }
+    },
+    handleMouseMove(e) {
+        this.x = e.offsetX
+        this.y = e.offsetY
     }
    }
 })
